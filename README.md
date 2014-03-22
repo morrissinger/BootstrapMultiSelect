75885d88af613c86209cb5abb663f6f29d052cec
Morris Singer <morrissinger@gmail.com>
2014-03-21 20:05:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+BootstrapMultiSelect
+====================
+http://morrissinger.github.io/BootstrapMultiSelect
+
+Who likes holding down CTRL?
+
+BootstrapMultiSelect is a javascript plugin for [Twitter Bootstrap 3](http://getbootstrap.com/) to help you create friendly multi-select components, created and maintained by [Morris Singer](http://morrissinger.com).
+
+Example
+-------
+...
+
+Quick start
+-----------
+
+Clone the repo, `git clone git://github.com/morrissinger/BootstrapMultiSelect.git`, or [download the latest release](https://github.com/morrissinger/BootstrapMutliSelect/zipball/master).
+
+Add CSS, or SASS to your project, as appropriate, and add JS. Then, just be sure any select component you want to use this on has data-toggle=multiselect.
+
+Author
+-------
+
+**Morris Singer**
+
++ http://twitter.com/morrissinger
++ http://github.com/morrissinger
+
+
+Copyright and license
+---------------------
+
+Copyright 2014 Morris Singer
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this work except in compliance with the License.
+You may obtain a copy of the License in the LICENSE file, or at:
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ js/bootstrap-multiselect.js
@@ -0,0 +1,76 @@
+/* ==========================================================
+ * bootstrap-multiselect.js
+ * https://github.com/morrissinger/BootstrapMultiSelect
+ * ==========================================================
+ * Copyright 2012 Morris Singer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
++function ($) {
+
+  'use strict';
+
+  $.fn.multiSelect = function(params) {
+
+    var defaults = {},
+    		$el = $(this).
+    		params = $.extend(defaults, params);
+    
+    $el.wrap("<div class=\"multiselect-wrapper\"></div>")
+
+	  var wrapper = $el.parent(),
+  		  $wrapper = $(wrapper);
+
+	  wrapper.append("<div class=\"multiselect-selector\"></div>");
+
+	  var selector = $wrapper.find(".multiselect-selector"),
+	      $selector = $(selector);
+
+	  options.each(function(optionIndex, option) {
+	    if (option.value !== "") {
+	      optionHtml = document.createElement("a");
+	      optionHtml.href="#";
+	      optionHtml.className = "multiselect-selector-option";
+	      optionHtml.data = {id: option.value};
+	      optionHtml.innerHTML = option.text;
+
+	      if ($(option).attr('selected') === "selected") {
+	      	optionHtml.className = optionHtml.className + " selected";
+	      }
+
+	      $selector.append(optionHtml);
+
+	      $(optionHtml).on('click', function(e) {
+	      	e.preventDefault();
+	      	if ($(this).hasClass("selected")) {
+	      		$(this).removeClass("selected");
+	      		option.selected = "";
+	      	} else {
+	      		$(this).addClass("selected");
+	      		option.selected = "selected";
+	      	}
+	      });
+	    }
+	  });
+	  
+	  $el.hide();
+  };
+
+  $('select[data-toggle=multiselect]').each(function(){
+    var $select;
+    $select = $(this);
+    $select.multiSelect($select.data());
+  });
+    
+}(window.jQuery);
